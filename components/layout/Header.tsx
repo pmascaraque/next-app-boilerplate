f711927c57f818bf43387c41db7731d1be1ff467
Pablo Moreno <ssm.pablo@gmail.com>
2022-08-22 14:13:39 +0200

    footer tag in header, ups

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Header() {
   return (
-    <footer>
+    <header>
       <p>
         Header
       </p>
-    </footer>
+    </header>
   );
 }
